b4e5460579e448511578133a3a7dd4205579632a
Fede Risetti <115749116+risettifede@users.noreply.github.com>
2023-10-17 19:07:55 -0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,12 @@ const Footer = () => {
     <footer className='bg-gray-100 font-mont text-black justify-center items-center text-center py-4 px-4 lg:py-8 '>
       <div className='max-w-7xl mx-auto'>
         <div className='flex flex-col items-center space-y-4 lg:flex-row lg:justify-between lg:space-y-0'>
-          <div className='text-xs mb-4 lg:mb-0'>
+          {/* <div className='text-xs mb-4 lg:mb-0'>
             <span className='lg:text-md font-extrabold'>KARROT © 2023</span> |
             Todos los derechos reservados
-          </div>
+          </div> */}
 
-          <div className='mb-4 lg:mb-0 xxxl:-ml-[23%]'>
+          {/* <div className='mb-4 lg:mb-0 xxxl:-ml-[23%]'>
             <a href='#' className='text-xs hover:underline'>
               Política de privacidad
             </a>
@@ -18,7 +18,7 @@ const Footer = () => {
             <a href='#' className='text-xs hover:underline'>
               Términos y condiciones
             </a>
-          </div>
+          </div> */}
 
           <div className='space-x-4'>
             <a
